8bc83eafd86a37cefd8093d56a29721bdbe05740
CyberTempest1w <samet102a@gmail.com>
2025-11-23 22:43:11 +0300

    veri çekme ayarları yapıldı

--- src/pages/user/HomeUser.jsx
+++ src/pages/user/HomeUser.jsx
@@ -9,7 +9,7 @@ function HomeUser() {
   const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
-    fetch("/data/data.json")
+    fetch(`${import.meta.env.BASE_URL}data/data.json`)
       .then((res) => {
         if (!res.ok) throw new Error("HTTP error " + res.status);
         return res.json();
