2fb098baf7edc92a19af0412215d06d241cb0373
CyberTempest1w <samet102a@gmail.com>
2025-11-23 22:14:59 +0300

    GitHub Pages için yeni ayarlar eklendi

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/samet1qwer.io">
     <App />
   </BrowserRouter>
 );
