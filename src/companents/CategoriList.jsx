4759f2b6424a5975592f5071b5e9c3ed9fe8e8b8
CyberTempest1w <samet102a@gmail.com>
2025-11-24 20:16:59 +0300

    data settings

--- src/companents/CategoriList.jsx
+++ src/companents/CategoriList.jsx
@@ -1,14 +1,12 @@
 const categoriesData = [
   { name: "Tümü", icon: null, isActive: true },
-  {
-    name: "Başlangıçlar",
-    isActive: false,
-  },
+
   {
     name: "Ana_Yemekler",
     isActive: false,
   },
   { name: "İçecekler", isActive: false },
   { name: "Tatlılar", isActive: false },
+  { name: "Webci_Menu", isActive: false },
 ];
 export default categoriesData;
